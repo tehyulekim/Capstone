0b5701b3952c44d3380aa3ebb6d45c4f91434de4
Teh Yule Kim <tehyulekim@gmail.com>
2019-09-22 23:35:27 +1200

    week 8 presentation

--- _Note/cli/_TEST.py
+++ _Note/cli/_TEST.py
@@ -1,3 +1,18 @@
-import os
-print(os.path.basename('fire1.py'))
+c1 = {'name': 'z', 'version': '1'}
+c2 = {'name': 'x', 'version': '2'}
+product1 = {'Name': 'product1'}
 
+# recipe == software release
+r1 = {
+    'Product': product1,
+    'Version Number': '1.2.3.4',
+    'Status': 'In Development',
+    'component_list': [
+        c1,
+        c2
+    ]
+}
+
+for x in r1['component_list']:
+    print(x)
+    print(x['name'])
