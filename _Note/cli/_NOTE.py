97bd3e66cd8b055a6c83b3970a4607db390decb5
Teh Yule Kim <tehyulekim@gmail.com>
2019-10-02 00:56:15 +1300

    cascade delete, default. Association id pkey, unique constraint API : check, view list, cname, cversion

--- _Note/cli/_NOTE.py
+++ _Note/cli/_NOTE.py
@@ -62,4 +62,36 @@ def de(name, version, target_dir='.'):
         print('zipfile extract complete')
 
 
+
+
+
+
+
+
+
+
+
+
+c1 = {'name': 'z', 'version': '1'}
+c2 = {'name': 'x', 'version': '2'}
+product1 = {'Name': 'product1'}
+
+# recipe == software release
+r1 = {
+    'Product': product1,
+    'Version Number': '1.2.3.4',
+    'Status': 'In Development',
+    'component_list': [
+        c1,
+        c2
+    ]
+}
+
+for x in r1['component_list']:
+    print(x)
+    print(x['name'])
+    
+    
+    
+    
 """
